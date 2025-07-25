e9b15f3c39a61e11ce6bc1fd18d2c061ea64e916
Nokutso <shxronjack@gmail.com>
2025-07-25 14:15:54 +0200

    firstsave code-with edit2

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + '/api', // adjust to your API path
+  baseURL: import.meta.env.VITE_API_URL, // adjust to your API path
   withCredentials: true, // optional, for cookies
 });
 
